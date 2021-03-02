4972ac04b9c6d513f6911acdf08d2ea861c59ea2
ibnshayed <emranibnshayed@gmail.com>
2021-03-02 19:51:35 +0600

    base list and detail templates added

--- Django 3 By Example/My Shop/shop/admin.py
+++ Django 3 By Example/My Shop/shop/admin.py
@@ -4,13 +4,13 @@ from .models import Category, Product
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
+    list_display = ['id','name', 'slug']
     prepopulated_fields = {'slug': ('name',)} # automatic populate slug field from name
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_display = ['id','name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available'] # edit from django admin site
     prepopulated_fields = {'slug': ('name',)}
